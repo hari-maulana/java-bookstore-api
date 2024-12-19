f01885c789776741656e4ce11c9e0a79106ea593
USER <USER@Hari-PC>
2024-12-19 09:37:28 +0700

    fix typo in api endpoint url

--- src/main/java/com/harimaulana/bookstore/controller/BookController.java
+++ src/main/java/com/harimaulana/bookstore/controller/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 public class BookController {
 	@Autowired
 	private BookRepository bookRepository;

48ec0e7ef0c6c5c7dc57fc99964044e898629853
USER <USER@Hari-PC>
2024-12-19 09:35:11 +0700

    create model for bookstore

---
+++ src/main/java/com/harimaulana/bookstore/model/Book.java
@@ -0,0 +1,47 @@
+package com.harimaulana.bookstore.model;
+
+import jakarta.persistence.*;
+	
+	@Entity // Annotate class as entity
+	public class Book {
+		@Id // Annotate id as primary key
+		@GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-generate id
+		private Long id;
+		private String title;
+		private String author;
+		private Double price;
+		
+		// Getter and Setter
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+		
+		public String getTitle() {
+			return title;
+		}
+		
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public String getAuthor() {
+			return author;
+		}
+		
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		
+		public Double getPrice() {
+			return price;
+		}
+		
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+	}
+

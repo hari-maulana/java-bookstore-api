a005097aa605272cc082c132b09430e0ecdad2f6
USER <USER@Hari-PC>
2024-12-19 10:01:39 +0700

    add comment for code clarity

--- src/main/java/com/harimaulana/bookstore/controller/BookController.java
+++ src/main/java/com/harimaulana/bookstore/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController // anotasi class sebagai rest controller
 @RequestMapping("/api/books")
 public class BookController {
 	@Autowired
